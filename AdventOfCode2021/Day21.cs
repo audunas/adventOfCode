8e4c86fa4e513fe118fea61c696f6b305db91951
audunas <audunasa@gmail.com>
2021-12-21 22:34:25 +0100

    Update Day21.cs
    All combos

--- AdventOfCode2021/Day21.cs
+++ AdventOfCode2021/Day21.cs
@@ -55,32 +55,64 @@ namespace AdventOfCode2021
             player1Position = 4;
             player2Position = 8;
 
-            currentDiceRoll = 1;
+            var player1Wins = 0l;
+            var player2Wins = 0l;
 
             while (player1Score < 21 && player2Score < 21)
             {
-                var player1Rolls = Enumerable.Range(currentDiceRoll, 3).Sum();
-                var player1NextSpace = NextSpace(player1Position, player1Rolls);
-                player1Score += player1NextSpace;
-                player1Position = player1NextSpace;
+                var allCombos = Combinations();
 
-                if (player1Score >= 21)
+                foreach (var combo1 in allCombos)
                 {
-                    currentDiceRoll += 3;
-                    break;
+                    foreach (var combo2 in allCombos)
+                    {
+                        var player1Rolls = combo1.Sum();
+                        var player1NextSpace = NextSpace(player1Position, player1Rolls);
+                        player1Score += player1NextSpace;
+                        player1Position = player1NextSpace;
+
+                        if (player1Score >= 21)
+                        {
+                            player1Wins++;
+                            break;
+                        }
+
+                        var player2Rolls = combo2.Sum();
+                        var player2NextSpace = NextSpace(player2Position, player2Rolls);
+                        player2Score += player2NextSpace;
+                        player2Position = player2NextSpace;
+
+                        if (player1Score >= 21)
+                        {
+                            player2Wins++;
+                            break;
+                        }
+
+                    }
                 }
-
-                var player2Rolls = Enumerable.Range(currentDiceRoll + 3, 3).Sum();
-                var player2NextSpace = NextSpace(player2Position, player2Rolls);
-                player2Score += player2NextSpace;
-                player2Position = player2NextSpace;
-
-                currentDiceRoll += 6;
             }
 
-            losingScore = Math.Min(player1Score, player2Score);
+            var winningScore = player1Wins > player2Wins ? player1Wins : player2Wins;
 
-            Console.WriteLine(losingScore * (currentDiceRoll - 1));
+            Console.WriteLine(winningScore);
+        }
+
+        private static List<List<int>> Combinations()
+        {
+            var combos = new List<List<int>>();
+
+            for (var i = 1; i<4; i++)
+            {
+                for (var j = 1; j < 4; j++)
+                {
+                    for (var k = 1; k < 4; k++)
+                    {
+                        combos.Add(new List<int>() {i, j, k });
+                    }
+                }
+            }
+
+            return combos;
         }
 
         private static int NextSpace(int currentSpace, int moves)
